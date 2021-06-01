fc247daa49f0e83d12bb71e2dc3a20f9cb45e8bc
mozammel-newroz <mozammel@newroztech.com>
2021-06-01 16:38:12 +0600

    working home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,12 +11,21 @@ import PlayCircleOutlineIcon from "@material-ui/icons/PlayCircleOutline";
 
 import automated from "../assets/images/automated.png";
 import ChasingActivityImage1 from "../assets/images/ChasingActivityImage1.png";
+import activity_image from "../assets/images/activity.png";
+import StartNow_image from "../assets/images/StartNow.png";
+import DemoVideo_image from "../assets/images/DemoVideo.png";
 
 const useStyles = makeStyles((theme) => ({
   wrapper: {
     padding: "0px 30px",
     [theme.breakpoints.down("sm")]: {
       padding: 0,
+    },
+  },
+  wrapper2: {
+    padding: "0px 50px",
+    [theme.breakpoints.down("sm")]: {
+      padding: 20,
     },
   },
   banner: {
@@ -29,6 +38,7 @@ const useStyles = makeStyles((theme) => ({
       padding: 0,
     },
   },
+
   banner_image: {
     flex: 1,
     padding: 30,
@@ -77,6 +87,51 @@ const useStyles = makeStyles((theme) => ({
     fontWeight: 600,
     marginBottom: 15,
   },
+  margin10: {
+    marginBottom: 30,
+  },
+  activity_wrapper2: {
+    background: "#E3EBFC",
+    borderRadius: 10,
+    padding: "40px 80px",
+    [theme.breakpoints.down("sm")]: {
+      padding: 20,
+    },
+  },
+  image: {
+    maxWidth: "100%",
+    [theme.breakpoints.down("sm")]: {
+      display: "none",
+    },
+  },
+  demo: {
+    background: "#E3EBFC",
+    borderRadius: 10,
+    padding: "60px 70px",
+    alignItems: "center",
+    [theme.breakpoints.down("sm")]: {
+      padding: 20,
+    },
+  },
+  demo_image: {
+    maxWidth: "100%",
+  },
+  demo_headline: {
+    marginBottom: 50,
+  },
+  activity2: {
+    display: "flex",
+    alignItems: "center",
+  },
+  improve: {
+    textAlign: "center",
+    marginTop: 40,
+    marginBottom: 40,
+  },
+  center_button: {
+    textAlign: "center",
+    marginTop: 40,
+  },
 }));
 
 const Home = () => {
@@ -85,7 +140,7 @@ const Home = () => {
     <>
       <CssBaseline />
       <Container maxWidth="lg">
-        <Grid container spacing={3} className={classes.wrapper}>
+        <Grid container spacing={5} className={classes.wrapper}>
           <Grid item lg={12}>
             <div className={classes.banner}>
               <div className={classes.banner_image}>
@@ -182,6 +237,123 @@ const Home = () => {
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit.
                 Voluptate natus consectetur temporibus
               </Typography>
+            </div>
+          </Grid>
+        </Grid>
+      </Container>
+      <Container maxWidth="lg">
+        <div className={classes.wrapper2}>
+          <Grid container spacing={5} className={classes.activity_wrapper2}>
+            <Grid item lg={12}>
+              <Typography variant="h4">
+                Activities that we are keeping track of
+              </Typography>
+            </Grid>
+            <div className={classes.activity2}>
+              <Grid item lg={6}>
+                <img className={classes.image} src={activity_image} alt="" />
+              </Grid>
+              <Grid item lg={6}>
+                <Typography variant="body1" className={classes.margin10}>
+                  <strong>Food: </strong> Lorem ipsum dolor sit amet,
+                  consectetur adipisicing elit. Cumque, a? Voluptatibus ut ea
+                  dolor perspiciatis voluptas quas
+                </Typography>
+                <Typography variant="body1" className={classes.margin10}>
+                  <strong>Food: </strong> Lorem ipsum dolor sit amet,
+                  consectetur adipisicing elit. Cumque, a? Voluptatibus ut ea
+                  dolor perspiciatis voluptas quas
+                </Typography>
+                <Typography variant="body1" className={classes.margin10}>
+                  <strong>Food: </strong> Lorem ipsum dolor sit amet,
+                  consectetur adipisicing elit. Cumque, a? Voluptatibus ut ea
+                  dolor perspiciatis voluptas quas
+                </Typography>
+                <Typography variant="body1" className={classes.margin10}>
+                  <strong>Food: </strong> Lorem ipsum dolor sit amet,
+                  consectetur adipisicing elit. Cumque, a? Voluptatibus ut ea
+                  dolor perspiciatis voluptas quas
+                </Typography>
+                <Typography variant="body1" className={classes.margin10}>
+                  <strong>Food: </strong> Lorem ipsum dolor sit amet,
+                  consectetur adipisicing elit. Cumque, a? Voluptatibus ut ea
+                  dolor perspiciatis voluptas quas
+                </Typography>
+                <Typography variant="body1" className={classes.margin10}>
+                  <strong>Food: </strong> Lorem ipsum dolor sit amet,
+                  consectetur adipisicing elit. Cumque, a? Voluptatibus ut ea
+                  dolor perspiciatis voluptas quas
+                </Typography>
+                <Typography variant="body1" className={classes.margin10}>
+                  <strong>Food: </strong> Lorem ipsum dolor sit amet,
+                  consectetur adipisicing elit. Cumque, a? Voluptatibus ut ea
+                  dolor perspiciatis voluptas quas
+                </Typography>
+              </Grid>
+            </div>
+          </Grid>
+        </div>
+      </Container>
+      <Container maxWidth="lg">
+        <Grid container spacing={5} className={`${classes.activity_wrapper}`}>
+          <Grid item lg={12}>
+            <Typography variant="h4">
+              Improve the quality of your program & <br /> give parents the
+              information they need
+            </Typography>
+            <Typography variant="h6" className={classes.improve}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor,
+              accusamus vitae? Quibusdam ad a nam nihil ea aperiam quod placeat
+              unde ducimus excepturi ratione porro dolore, rem provident.
+            </Typography>
+            <div className={classes.improve}>
+              <img src={StartNow_image} alt="" />
+            </div>
+            <div className={classes.center_button}>
+              <Button
+                variant="contained"
+                color="primary"
+                className={`${classes.banner_button} button`}
+              >
+                Start Now
+              </Button>
+            </div>
+          </Grid>
+        </Grid>
+      </Container>
+
+      <Container maxWidth="lg">
+        <Grid container spacing={5} className={classes.wrapper}>
+          <Grid item lg={12}>
+            <div className={classes.demo}>
+              <Typography variant="h4" className={classes.demo_headline}>
+                KinderCubby activity tracking demo
+              </Typography>
+              <img
+                className={classes.demo_image}
+                src={DemoVideo_image}
+                alt=""
+              />
+            </div>
+          </Grid>
+        </Grid>
+      </Container>
+
+      <Container maxWidth="lg">
+        <Grid container spacing={5} className={`${classes.activity_wrapper}`}>
+          <Grid item lg={12}>
+            <Typography variant="h4">
+              Get better insight & actionable information <br /> with
+              KinderCubby
+            </Typography>
+            <div className={classes.center_button}>
+              <Button
+                variant="contained"
+                color="primary"
+                className={`${classes.banner_button} button`}
+              >
+                Yes! Let’s try it.
+              </Button>
             </div>
           </Grid>
         </Grid>
