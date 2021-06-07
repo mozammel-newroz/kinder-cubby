27b035ed4599e0e2cbbad5a5659bf5d6c010bda9
mozammel-newroz <mozammel@newroztech.com>
2021-06-07 19:57:13 +0600

    price page complete

--- src/pages/Pricing.jsx
+++ src/pages/Pricing.jsx
@@ -6,178 +6,411 @@ import {
   Typography,
   Button,
 } from "@material-ui/core";
-import { makeStyles, useTheme } from "@material-ui/core/styles";
-import PlayCircleOutlineIcon from "@material-ui/icons/PlayCircleOutline";
+import { Link } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import Accordion from "@material-ui/core/Accordion";
+import AccordionSummary from "@material-ui/core/AccordionSummary";
+import AccordionDetails from "@material-ui/core/AccordionDetails";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
-import automated from "../assets/images/automated.png";
-import ChasingActivityImage1 from "../assets/images/ChasingActivityImage1.png";
-import activity_image from "../assets/images/activity.png";
-import StartNow_image from "../assets/images/StartNow.png";
-import DemoVideo_image from "../assets/images/DemoVideo.png";
+import Pricing from "../assets/images/Pricing.png";
 
 const useStyles = makeStyles((theme) => ({
+  wrapper1: {
+    padding: "60px 30px",
+    [theme.breakpoints.down("xs")]: {
+      padding: 10,
+    },
+  },
+  wrapper2: {
+    padding: "60px 70px",
+    background: "#E3EBFC",
+    borderRadius: 15,
+    [theme.breakpoints.down("xs")]: {
+      padding: 10,
+    },
+  },
   wrapper: {
     padding: "0px 30px",
     [theme.breakpoints.down("xs")]: {
       padding: 0,
     },
   },
-  wrapper2: {
-    padding: "0px 50px",
-    [theme.breakpoints.down("xs")]: {
-      padding: 20,
-    },
+  table: {
+    width: "99.99%",
   },
-  banner: {
-    background: "#E3EBFC",
-    borderRadius: 10,
-    padding: 50,
-    display: "flex",
-    alignItems: "center",
-    [theme.breakpoints.down("xs")]: {
-      marginTop: 10,
-      padding: 0,
-    },
+  table_cell: {
+    border: "1px solid #ddd",
+    fontSize: "1rem",
+    width: 400,
   },
-
-  banner_image: {
-    flexGrow: 1,
-    padding: 30,
-    [theme.breakpoints.down("xs")]: {
-      display: "none",
-    },
+  table_cell_big: {
+    border: "1px solid #FBC9BA",
+    background: "#FFF8F5",
+    fontSize: "1rem",
+    width: 400,
   },
-  banner_content: {
-    flexGrow: 1,
-    padding: 30,
-    [theme.breakpoints.down("xs")]: {
-      width: "100%",
-      padding: 20,
-    },
+  no_border: {
+    border: "0px solid #ddd",
+    fontSize: "1rem",
+    padding: 0,
+    margin: 0,
   },
-  banner_button: {
-    padding: "13px 35px  !important",
-  },
-  banner_headline: {
+  table_head: {
+    fontSize: "1.1rem",
     fontWeight: 600,
-    marginBottom: 80,
-    [theme.breakpoints.down("xs")]: {
-      marginBottom: 20,
-    },
+    background: "#E3EBFC",
   },
-  play: {
-    color: "#0B4295",
-    marginLeft: 10,
-    padding: "13px 35px  !important",
-    [theme.breakpoints.down("xs")]: {
-      marginLeft: 0,
-    },
+  table_head2: {
+    fontSize: "1.1rem",
+    fontWeight: 600,
   },
-  activity_wrapper: {
-    padding: "60px 120px",
-    [theme.breakpoints.down("xs")]: {
-      padding: "20px 0px",
-    },
-  },
-
-  activity_box: {
+  box: {
+    boxShadow: "0px 0px 1px 1px #eee",
     textAlign: "center",
+    marginBottom: 20,
+    marginTop: 5,
   },
-  activity_image: {
-    maxWidth: "100%",
-    padding: "0px 40px 15px 40px",
+  box_big: {
+    boxShadow: "1px 0px 5px 3px #ddd",
+    textAlign: "center",
+    marginBottom: 20,
+    marginTop: 5,
   },
-  activity_box_headline: {
-    fontSize: 22,
+  box_title: {
+    fontSize: "1.3rem",
     fontWeight: 600,
-    marginBottom: 15,
+    borderBottom: "2px solid #ddd",
+    padding: 7,
   },
-  margin10: {
-    marginBottom: 30,
+  box_title_big: {
+    fontSize: "1.3rem",
+    fontWeight: 600,
+    borderBottom: "2px solid #FBC9BA",
+    padding: 7,
   },
-  activity_wrapper2: {
-    background: "#E3EBFC",
-    borderRadius: 10,
-    padding: "40px 80px",
-    [theme.breakpoints.down("xs")]: {
-      padding: 20,
-    },
+  box_price: {
+    fontSize: "2.5rem",
+    fontWeight: 600,
+    padding: 25,
   },
-  image: {
-    maxWidth: "100%",
-    [theme.breakpoints.down("xs")]: {
-      display: "none",
-    },
+  box_button: {
+    padding: 25,
   },
-  demo: {
-    background: "#E3EBFC",
-    borderRadius: 10,
-    padding: "60px 70px",
-    alignItems: "center",
-    [theme.breakpoints.down("xs")]: {
-      padding: 20,
-    },
+  h6: {
+    marginTop: 15,
+    marginBottom: 0,
   },
-  demo_image: {
-    maxWidth: "100%",
-  },
-  demo_headline: {
+  question_title: {
+    fontSize: "1.6rem",
+    fontWeight: 600,
     marginBottom: 50,
-  },
-  activity2: {
-    display: "flex",
-    alignItems: "center",
-  },
-  improve: {
     textAlign: "center",
-    marginTop: 40,
-    marginBottom: 40,
   },
-  center_button: {
+  question_title_bottom: {
+    fontSize: "1.6rem",
+    fontWeight: 600,
+    margin: "50px 0px 20px 0px",
     textAlign: "center",
-    marginTop: 40,
   },
+  center: {
+    textAlign: "center",
+  },
+  question: {
+    fontSize: "1.1rem",
+    fontWeight: 600,
+  },
+  answer: {
+    fontSize: "1.1rem",
+  },
+  accordion: {
+    background: "transparent",
+    boxShadow: "0px 0px 0px  #000",
+    borderBottom: "1px solid #C6CFDC",
+  },
+  link: {
+    textDecoration: "underline",
+    color: "#0B4295",
+  },
+  bottom_button: {
+    textAlign: 'center',
+    marginTop: 30
+  }
 }));
 
 const Home = () => {
   const classes = useStyles();
   return (
     <>
-      {/* <CssBaseline /> */}
+      <Container maxWidth="lg">
+        <Grid container className={classes.wrapper1}>
+          <Grid item lg={12}>
+            <Typography variant="h4">Simple & Transparent Pricing</Typography>
+            <Typography variant="h6" className={classes.h6}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+              blanditiis suscipit facere adipisci vel culpa reiciendis,
+              voluptatum provident dolore numquam.
+            </Typography>
+          </Grid>
+        </Grid>
+      </Container>
       <Container maxWidth="lg">
         <Grid container className={classes.wrapper}>
           <Grid item lg={12}>
-            <div className={classes.banner}>
-              <div className={classes.banner_image}>
-                <img src={automated} alt="" />
-              </div>
-              <div className={classes.banner_content}>
-                <Typography variant="h3" className={classes.banner_headline}>
-                  Automated
+            <TableContainer component={Paper}>
+              <Table className={classes.table}>
+                <TableHead>
+                  <TableRow>
+                    <TableCell className={classes.no_border}>
+                      <img src={Pricing} alt="" />
+                    </TableCell>
+                    <TableCell align="center" className={classes.no_border}>
+                      <div className={classes.box}>
+                        <div className={classes.box_title}>Free</div>
+                        <div className={classes.box_price}>$0</div>
+                        <div className={classes.box_text}>
+                          <Typography variant="body2">
+                            Always free for 10 students
+                          </Typography>
+                          <Typography variant="body2">
+                            1 Staff member
+                          </Typography>
+                        </div>
+                        <div className={classes.box_button}>
+                          <Button
+                            variant="outlined"
+                            color="primary"
+                            className="button"
+                          >
+                            Get Started
+                          </Button>
+                        </div>
+                      </div>
+                    </TableCell>
+                    <TableCell align="center" className={classes.no_border}>
+                      <div className={classes.box_big}>
+                        <div className={classes.box_title_big}>Free</div>
+                        <div className={classes.box_price}>$0</div>
+                        <div className={classes.box_text}>
+                          <Typography variant="body2">
+                            Always free for 10 students
+                          </Typography>
+                          <Typography variant="body2">
+                            1 Staff member
+                          </Typography>
+                        </div>
+                        <div className={classes.box_button}>
+                          <Button
+                            variant="contained"
+                            color="primary"
+                            className="button"
+                          >
+                            Get Started
+                          </Button>
+                        </div>
+                      </div>
+                    </TableCell>
+                    <TableCell align="center" className={classes.no_border}>
+                      <div className={classes.box}>
+                        <div className={classes.box_title}>Free</div>
+                        <div className={classes.box_price}>$0</div>
+                        <div className={classes.box_text}>
+                          <Typography variant="body2">
+                            Always free for 10 students
+                          </Typography>
+                          <Typography variant="body2">
+                            1 Staff member
+                          </Typography>
+                        </div>
+                        <div className={classes.box_button}>
+                          <Button
+                            variant="outlined"
+                            color="primary"
+                            className="button"
+                          >
+                            Get Started
+                          </Button>
+                        </div>
+                      </div>
+                    </TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableHead>
+                  <TableRow>
+                    <TableCell
+                      align="center"
+                      colSpan="5"
+                      className={classes.table_head}
+                    >
+                      Features
+                    </TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  <TableRow>
+                    <TableCell className={classes.table_cell}>
+                      Number of Students
+                    </TableCell>
+                    <TableCell align="center" className={classes.table_cell}>
+                      10
+                    </TableCell>
+                    <TableCell
+                      align="center"
+                      className={classes.table_cell_big}
+                    >
+                      60
+                    </TableCell>
+                    <TableCell align="center" className={classes.table_cell}>
+                      Unlimited
+                    </TableCell>
+                  </TableRow>
+
+                  <TableRow>
+                    <TableCell className={classes.table_cell}>
+                      Number of Students
+                    </TableCell>
+                    <TableCell align="center" className={classes.table_cell}>
+                      10
+                    </TableCell>
+                    <TableCell
+                      align="center"
+                      className={classes.table_cell_big}
+                    >
+                      60
+                    </TableCell>
+                    <TableCell align="center" className={classes.table_cell}>
+                      Unlimited
+                    </TableCell>
+                  </TableRow>
+                </TableBody>
+                <TableHead>
+                  <TableRow>
+                    <TableCell
+                      align="center"
+                      colSpan="5"
+                      className={classes.table_head2}
+                    >
+                      Activity tracking
+                    </TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  <TableRow>
+                    <TableCell className={classes.table_cell}>
+                      Number of Students
+                    </TableCell>
+                    <TableCell align="center" className={classes.table_cell}>
+                      10
+                    </TableCell>
+                    <TableCell
+                      align="center"
+                      className={classes.table_cell_big}
+                    >
+                      60
+                    </TableCell>
+                    <TableCell align="center" className={classes.table_cell}>
+                      Unlimited
+                    </TableCell>
+                  </TableRow>
+
+                  <TableRow>
+                    <TableCell className={classes.table_cell}>
+                      Number of Students
+                    </TableCell>
+                    <TableCell align="center" className={classes.table_cell}>
+                      10
+                    </TableCell>
+                    <TableCell
+                      align="center"
+                      className={classes.table_cell_big}
+                    >
+                      60
+                    </TableCell>
+                    <TableCell align="center" className={classes.table_cell}>
+                      Unlimited
+                    </TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </Grid>
+        </Grid>
+      </Container>
+      <Container maxWidth="lg">
+        <Grid container className={classes.wrapper1}>
+          <Grid item lg={12} className={classes.center}>
+            <Button variant="outlined" color="primary" className="button">
+              Learn More About Features
+            </Button>
+          </Grid>
+        </Grid>
+      </Container>
+      <Container maxWidth="lg">
+        <Grid container className={classes.wrapper2}>
+          <Grid item lg={12}>
+            <Typography className={classes.question_title}>
+              Frequently asked questions
+            </Typography>
+            <Accordion className={classes.accordion}>
+              <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                <Typography className={classes.question}>
+                  Accordion 1
                 </Typography>
-                <Typography style={{ fontSize: 24, marginBottom: 30 }}>
-                  Sed a magna semper, porta purus eu, ullamcorper ligula. Nam
-                  sit amet consectetur
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography className={classes.answer}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
+                  eget.
                 </Typography>
-                <div>
-                  <Button
-                    variant="contained"
-                    color="primary"
-                    className={`${classes.banner_button} button`}
-                  >
-                    Try it Free
-                  </Button>
-                  <Button
-                    // variant="contained"
-                    color="secondary"
-                    className={`${classes.play} button`}
-                    startIcon={<PlayCircleOutlineIcon />}
-                  >
-                    Watch In Action
-                  </Button>
-                </div>
-                <div></div>
-              </div>
+              </AccordionDetails>
+            </Accordion>
+            <Accordion className={classes.accordion}>
+              <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                <Typography className={classes.question}>
+                  Accordion 2
+                </Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography className={classes.answer}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
+                  eget.
+                </Typography>
+              </AccordionDetails>
+            </Accordion>
+            <Typography className={classes.question_title_bottom}>
+              Still have more questions?
+            </Typography>
+            <Typography className={classes.center}>
+              Contact our{" "}
+              <Link to="/#" className={classes.link}>
+                support
+              </Link>{" "}
+            </Typography>
+          </Grid>
+        </Grid>
+      </Container>
+      <Container maxWidth="lg">
+        <Grid container className={classes.wrapper1}>
+          <Grid item lg={12}>
+            <Typography variant="h4">Ready to take the next step</Typography>
+            <Typography variant="h6" className={classes.h6}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+              blanditiis suscipit facere adipisci vel culpa reiciendis,
+              voluptatum provident dolore numquam.
+            </Typography>
+            <div className={classes.bottom_button}>
+              <Button variant="contained" color="primary" className="button">
+                Try It Free
+              </Button>
             </div>
           </Grid>
         </Grid>
